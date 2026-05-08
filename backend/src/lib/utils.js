ec36fdf9ec7f35fa8d4cc41084d7dcfc7cc844e4
abdulgilani <ah-gilani@hotmail.com>
2026-05-08 15:49:38 -0400

    Created generateToken file

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (userId, res) => {
+
+    const token = jwt.sign({ id: userId }, process.env.JWT, {
+        expiresIn: '7d'
+    })
+
+    res.cookie('jwt', token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict'
+    })
+
+    return token;
+}
