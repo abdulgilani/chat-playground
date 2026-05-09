7f73b65da93905e83b4db1eb6c9d961783a402a2
abdulgilani <ah-gilani@hotmail.com>
2026-05-09 12:23:30 -0400

    Created the singup controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -24,13 +24,23 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
 
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fulllName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      })
     } else {
       return res.status(400).json({ message: "Invalid user data" });
     }
 
   } catch (error) {
-
+    console.log("Error in signup controller: ", error);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
