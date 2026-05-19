41bb002290837a0067a55a844a40fd5bbd44f1b7
abdulgilani <ah-gilani@hotmail.com>
2026-05-19 09:31:43 -0400

    Build Sidebar.jsx

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,27 +1,54 @@
-import { create } from zustand;
-import { axiosInstance } from '../utils/axios';
+import { create } from "zustand";
+import { axiosInstance } from "../utils/axios";
+import toast from "react-hot-toast";
 
 export const useChatStore = create((set, get) => ({
-    users: [],
-    isUserLoading: false,
+  users: [],
+  messages: [],
+  selectedUser: null,
+  isUserLoading: false,
+  isMessageLoading: false,
 
-    getUsers: async () => {
-        set({isUserLoading: true })
-        try{
-            const res = await axiosInstance.get("/messages/users");
-            set({ users: res.data });
-        } catch (error) {
-            toast.error(error.response.data.message);
-        } finally {
-            set({ isUserLoading: false});
-        }
-    },
+  getUsers: async () => {
+    set({ isUserLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Failed at getting users");
+    } finally {
+      set({ isUserLoading: false });
+    }
+  },
 
-    getMessages: async() => {
+  getMessages: async (userId) => {
+    set({ isMessageLoading: false });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(
+        error?.response?.data?.message || "Failed at getting messages",
+      );
+    } finally {
+      set({ isMessageLoading: false });
+    }
+  },
 
-    },
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.put(
+        `/messages/send/${selectedUser._id}`,
+        messageData,
+      );
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(
+        error?.response?.data?.message || "Failed at sending a message",
+      );
+    }
+  },
 
-    sendMessages: async() => {
-
-    },
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
