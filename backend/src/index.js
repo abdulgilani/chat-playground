961e9ec6de3034495780a5cf7c58a5ead13edca0
abdulgilani <ah-gilani@hotmail.com>
2026-05-23 16:45:14 -0400

    Changed backend CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-playground-2mq9.onrender.com",
     credentials: true,
   }),
 );
